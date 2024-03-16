cfb907bbbd32dcd16a4b2a6f331bc0cd955de70e
mouradghaly <101068633+mouradghaly@users.noreply.github.com>
2024-03-16 18:24:51 +0200

    Update views.py

--- registration/views.py
+++ registration/views.py
@@ -35,7 +35,7 @@ def registration(request):
             p.save()
             return HttpResponse("OK")
     else:
-        return render(request,"/Users/mouradghaly/python-stuff/iftarapp/registration/templates/registration.html", {
+        return render(request,"registration/registration.html", {
             "form": reg
         })
 
